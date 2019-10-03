834b685d0cccde84d3a7f6f829b0244759dcb3be
Valerria Hutsuliak <valeria.gutsuliak@gmail.com>
2019-10-03 17:34:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,9 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-
+ Diff:
+       @@ -1,2 +1 @@	       @@ -1,2 +1 @@
+       -Hi, my name is Josh.	       -Hi, my name is Josh.
 
 
 
